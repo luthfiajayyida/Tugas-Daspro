8110c3d9cdd8ee2ab4900166bf77fd717a265337
luthfiajayyida <113040078+luthfiajayyida@users.noreply.github.com>
2022-09-07 22:00:53 +0700

    tugas 1

---
+++ Tugas-Daspro/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //membuat tugas 1 berisi biodata
+    cout << "Hello People" << endl;
+    cout << "Namaku Luthfia Jayyida Ainaya Fatiha atau bisa dipanggl Ai" << endl;
+    cout << "Aku berasal dari Rembang Jawa Tengah yang khas dengan tambak dan pantainya" << endl;
+    cout << "Zodiakku Virgo nih, tepatnya aku lahir pada tanggal 9 September 2004" << endl;
+    cout << "Hobiku dengerin musik dan bernyanyi, terutama lagu barat dan kpop karena lagu indo diputer cuman pas galau aja hehehe :p" << endl;
+    cout << "Apalagi ya..." << endl;
+    cout << "oiya, aku suka sheila on7, seventeen cabang Korea dan Indonesia" << endl;
+    cout << "Golongan darahku A makanya ipk ku harus 4 biar cocok sama golongan darahku hewhewhew" << endl;
+    cout << "Udah cukup nih kayanya" << endl;
+    cout << "Abang Riyan beli serabi" << endl;
+    cout << "Cukup sekian, makasiii" << endl;
+    return 0;
+}
