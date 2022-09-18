899f4236a436856453ac307b16a54244cccdeda5
Luthfia Jayyida Ainaya F <113040078+luthfiajayyida@users.noreply.github.com>
2022-09-18 19:02:28 +0700

    Tugas 2

---
+++ Tugas 2 Luthfia Jayyida/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    //print(text\n);
+
+    int u = 20;
+    float t = 155;
+    char ub = 'm';
+    string ubs ="xl";
+
+    cout << "umur saya: " << u;
+    cout << endl;
+    cout <<"tinggi saya: " << t;
+    cout <<"\nukuran baju: " << ubs;
+
+    int a;
+    cout <<"\nmasukkan angka anda: ";
+    cin >> a;
+    cout <<"angka anda: " << a + u ;
+
+    return 0;
+}
