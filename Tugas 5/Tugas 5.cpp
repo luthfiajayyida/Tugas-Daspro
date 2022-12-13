6ade576380da2b61722f4b73ce12fe18f0e2ac4e
Luthfia Jayyida Ainaya F <113040078+luthfiajayyida@users.noreply.github.com>
2022-12-13 23:54:48 +0700

    Tugas 5

---
+++ Tugas 5/Tugas 5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+  	/*
+  		//bios
+	string nama;
+  	float umur;
+  	cout << "nama saya : ";
+  	cin >> nama;
+  	cout << "umur saya : ";
+  	cin >> umur;
+  	
+  	cout << "nama saya adalah " << nama << "dan umur saya " << umur;
+  	*/
+  	
+  	
+  	/*
+  		//segi tiga
+	float t, a, h1;
+  		cout << "masukkan alas : ";
+		  cin >> a;
+  		cout << "masukkan tinggi : ";
+		  cin >> t;
+  		h1 = a*t/2;
+  		cout << "jadi luas segi tiga adalah : " << h1;
+  	*/
+  	
+  	
+  	/*
+  		//persegi panjang
+  	float p, l, h1;
+  		cout << "masukkan panjang : ";
+  			cin >> p;
+  		cout << "masukkan lebar : ";
+  			cin >> l;
+  		h1 = p*l;
+  		cout << "jadi luas persegi panjang adalah : " << h1;
+  	*/
+  	
+  	
+  	system ('cls')
+  	//kom=ndisi (else - if - else if)
+  	bool 
+  	
+  	return 0;
+  	
+    }
